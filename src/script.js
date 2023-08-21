d8696c52ca6740307f5ace55d3517ab67412acdd
Luiz Pachioni <lopachioni@gmail.com>
2023-08-21 10:22:13 -0300

    criando repositorio do projeto

---
+++ src/script.js
@@ -0,0 +1,58 @@
+let visor = document.getElementById("visor");
+
+function adicionar(tecla) {
+  visor.value = visor.value + tecla;
+}
+
+function limpezatotal() {
+  visor.value = "";
+}
+
+function calcular() {
+  let numeros = visor.value.split(/[^0-9]/)
+  let operadores = visor.value.split(/[0-9]/)
+  let operador
+  operadores.forEach(x => {
+    if (x != "")
+      operador = x
+  });
+
+  visor.value = Calculadora(numeros[0], numeros[1], operador)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function Calculadora(numero1, numero2, operador) {
+  //ESCREVA SEU CÓDIGO AQUI!!
+  let resultado
+  let num1 = parseInt(numero1)
+  let num2 = parseInt(numero2)
+
+  switch (operador) {
+    case "+":
+      resultado = num1 + num2
+      break;
+    case "-":
+      resultado = num1 - num2
+      break;
+    case "/":
+      resultado = num1 / num2
+      break;
+    case "*":
+      resultado = num1 * num2
+      break;
+    case "e":
+      let contador = 1
+      resultado = num1 * num1
+      while (contador < num2 - 1) {
+        resultado = resultado * num1
+        contador++
+      }
+      break;
+  }
+
+  if (resultado == undefined || resultado > 1000000)
+    resultado = "ERRO"
+
+  return resultado
+}

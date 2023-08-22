d4fc8bdb5d03644dc62b64dd14feda1cf996b4d1
Luiz Pachioni <lopachioni@gmail.com>
2023-08-22 10:58:28 -0300

    finalizando estrutura do projeto

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ESCREVA SEU CÓDIGO AQUI!!
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 let visor = document.getElementById("visor");
 
 function adicionar(tecla) {
@@ -18,41 +27,4 @@ function calcular() {
   });
 
   visor.value = Calculadora(numeros[0], numeros[1], operador)
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function Calculadora(numero1, numero2, operador) {
-  //ESCREVA SEU CÓDIGO AQUI!!
-  let resultado
-  let num1 = parseInt(numero1)
-  let num2 = parseInt(numero2)
-
-  switch (operador) {
-    case "+":
-      resultado = num1 + num2
-      break;
-    case "-":
-      resultado = num1 - num2
-      break;
-    case "/":
-      resultado = num1 / num2
-      break;
-    case "*":
-      resultado = num1 * num2
-      break;
-    case "e":
-      let contador = 1
-      resultado = num1 * num1
-      while (contador < num2 - 1) {
-        resultado = resultado * num1
-        contador++
-      }
-      break;
-  }
-
-  if (resultado == undefined || resultado > 1000000)
-    resultado = "ERRO"
-
-  return resultado
 }
